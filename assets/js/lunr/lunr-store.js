13871b6abff38062c2382c36c74d10d5edcd17f8
aminizadyar <87753648+aminizadyar@users.noreply.github.com>
2025-05-02 17:03:01 +0000

    Deploying to gh-pages from @ aminizadyar/aminizadyar.github.io@81e632e7512fde552f5812ab96b6e76394ccab9c 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Contact",
+        "excerpt":"Feel free to drop me a line:      📧 Email: amin@example.com   💬 Twitter: @aminizadyar   🐙 GitHub: aminizadyar  ","categories": [],
+        "tags": [],
+        "url": "/contact/",
+        "teaser": null
+      },{
+        "title": "Research",
+        "excerpt":"My Research   …  ","categories": [],
+        "tags": [],
+        "url": "/research/",
+        "teaser": null
+      },]
